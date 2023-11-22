8d26c718528887481b02596113dc8bd6da1da72c
anamargariida <amd16@hi.is>
2023-11-22 18:32:01 +0000

    Breytti engu

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function renderFrontPage() {
             displayProducts(data.items);
         })
         .catch(error => console.error('Error:', error));
-    fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/categories?limit=6')
+    fetch('https://vef1-2023-h2-api-791d754dda5b.herokuapp.com/categories?limit=12')
         .then(response => response.json())
         .then(data => {
             displayCategories(data.items);
@@ -22,12 +22,14 @@ function displayProducts(products) {
         const productDiv = document.createElement('div');
         productDiv.classList.add('box');
         productDiv.innerHTML = `
+
             <img src="${product.image}" alt="${product.title}" style="max-width: 100px;">
             <h3>${product.title}</h3>
             <p>${product.category_title}</p>
             <p>${product.price} kr.-</p>
             <a href="/product/${product.id}">View Product</a>
         `;
+        // @ts-ignore
         container.appendChild(productDiv);
     });
 }
@@ -36,10 +38,11 @@ function displayCategories(categories) {
     const container = document.getElementById('categories-container');
     categories.forEach(category => {
         const categoriesDiv = document.createElement('div');
-        categoriesDiv.classList.add('box');
+        categoriesDiv.classList.add('box1');
         categoriesDiv.innerHTML = `
             <a href="/categories/${category.id}"><h3>${category.title}</h3></a>
         `;
+        // @ts-ignore
         container.appendChild(categoriesDiv);
     })
 }
